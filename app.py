0d4ff637d3d40ac2e63108111fdee714c59d78ff
Mehdi Ordikhani <mehdi.ordikhani@samesystem.com>
2023-09-10 11:46:35 +0200

    initial commit

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import wikipedia
 
 wikipedia = WikipediaAPIWrapper()
 
-# Set favicon
 st.set_page_config(page_title="Streamlit App", page_icon="static/res/favicon.png")
 
 st.markdown(
@@ -18,7 +17,9 @@ st.markdown(
     </style>
     <a href="https://pythonpythonme.netlify.app/index.html">
     <div class="center-image">
+    <!--
     <img src="https://pythonpythonme.netlify.app/PythonPythonME.png" alt="Header image">
+    -->
     </div>
     </a>
     <p></p>
@@ -51,7 +52,6 @@ question_input = st.text_input("Question:")
 if question_input:
     # Extract keywords from the question input
     keywords = question_input.split()
-
     # Fetch context information using the Wikipedia toolkit based on keywords
     wikipedia = WikipediaAPIWrapper()
     context_input = wikipedia.run(' '.join(keywords))
@@ -91,27 +91,25 @@ st.markdown(
     </style>
     <body>
         <div class="center-image">
-            <h4>Anoop Johny 🤖</h4>
+            <h4>Mehdi Ordikhani 🤖</h4>
         </div>
         <div class="center-image">
             <h4>Follow Me</h4>
         </div>
         <div class="center-image">
             <ul class="social-icons">
-                <li><a href="https://www.linkedin.com/in/anoop-johny-30a746181/"><img src="https://pythonpythonme.netlify.app/static/res/linkedin.png" width="55" height="55" alt="LinkedIn"></a></li>
-                <li><a href="https://github.com/flyfir248"><img src="https://pythonpythonme.netlify.app/static/res/github.png" width="55" height="55" alt="GitHub"></a></li>
-                <li><a href="https://pythonpythonme.netlify.app/index.html"><img src="https://pythonpythonme.netlify.app/static/res/web.png" width="55" height="55" alt="Website"></a></li>
-                <li><a href="https://medium.com/@anoopjohny2000"><img src="https://pythonpythonme.netlify.app/static/res/medium.png" width="55" height="55" alt="Medium"></a></li>
-                <li><a href="https://www.kooapp.com/profile/anoop2DEVLJ"><img src="https://www.kooapp.com/_next/static/media/logoKuSolidOutline.1f4fa971.svg" width="55" height="55" alt="The Koo App" width="55" height="55"></a></li>
-                <a href="https://www.kaggle.com/anoopjohny"><img src="https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/189_Kaggle-1024.png" alt="The Kaggle App" width="55" height="55"></a>
+                <li><a href="https://www.linkedin.com/in/mehdi-ordikhani-seyedlar-11349b103/"><img src="https://img.freepik.com/premium-vector/linkedin-logo_578229-227.jpg" width="55" height="55" alt="LinkedIn"></a></li>
+                <li><a href="https://github.com/mehdiordi"><img src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png" width="55" height="55" alt="GitHub"></a></li>
             </ul>
         </div>
         <footer class="footer">
             <div class="container">
                 <div class="row">
                     <div class="center-image">
+                    <!--
                         <p class="text-muted">© 2023-2024 PythonPythonME.</p>
                         <p>All rights reserved.</p>
+                    -->
                     </div>
                 </div>
             </div>
